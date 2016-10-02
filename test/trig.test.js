4da160cd72db38671a0b96cea2eb9d0dead7a304
Timothy Sugar <timothysugar@gmail.com>
2016-10-02 10:18:16 +0100

    Reworked tests for javascript only

--- test/trig.test.js
+++ test/trig.test.js
@@ -1,36 +1,8 @@
 describe('trig', function(){
 
-	beforeEach(function() {
-		var fixture = '<div id="fixture">' + 
-		'<input id="a" type="text">' + 
-		'<input id="b" type="text">' + 
-		'<input id="c" type="text">' + 
-		'<input id="d" type="text">' + 
-		'<input id="path" type="button" value="format path string">' +
-		'Result: <span id="result" /></div>';
-
-		document.body.insertAdjacentHTML(
-			'afterbegin', 
-			fixture);
-	});
-
-	// remove the html fixture from the DOM
-	afterEach(function() {
-		document.body.removeChild(document.getElementById('fixture'));
-	});
-
-	// call the init function of trig to register DOM elements
-	beforeEach(function() {
-		window.trig.init();
-	});
-
-	it('should return a path formatted string', function() {
-		document.getElementById('a').value = 1;
-		document.getElementById('b').value = 2;
-		document.getElementById('c').value = 3;
-		document.getElementById('d').value = 4;
-		document.getElementById('path').click();
-		expect(document.getElementById('result').innerHTML).toBe('m 1 2 m 3 4 z');
-	});
+	it('should return a path string formatted for a triangle', function() {
+		var pathString = toTrianglePath(1, 2, 3, 4);
+		expect(pathString).toBe('m 1 2 m 3 4 z');
+	})
 
 })
--- trig.js
+++ trig.js
@@ -1,17 +1,5 @@
 'use strict'
 
-window.trig = window.trig || {};
-
-(function() {
-	var createPathString = function() {
-		var a = document.getElementById('a').value;
-		var b = document.getElementById('b').value;
-		var c = document.getElementById('c').value;
-		var d = document.getElementById('d').value;
-		document.getElementById('result').innerHTML = 'm ' + a + ' ' + b + ' m ' + c + ' ' + d + ' ' + 'z';
-	};
-
-	window.trig.init = function() {
-		document.getElementById('path').addEventListener('click', createPathString);
-	};
-})();
+function toTrianglePath(a, b, c, d) {
+	return 'm ' + a + ' ' + b + ' m ' + c + ' ' + d + ' ' + 'z';
+}
