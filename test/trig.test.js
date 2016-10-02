67b2933641b7a5d02f52805de6bfb822e906b36a
Timothy Sugar <timothysugar@gmail.com>
2016-10-02 10:48:29 +0100

    use strict in tests

--- test/trig.test.js
+++ test/trig.test.js
@@ -1,17 +1,19 @@
+'use strict'
+
 describe('trig', function(){
 
 	it('should return a path string formatted for a triangle for positive numbers', function() {
 		var pathString = toTrianglePath(1, 2, 3, 4);
 		expect(pathString).toBe('m 1 2 m 3 4 z');
-	})
+	});
 
 	it('should return a path string formatted for a triangle for positive numbers', function() {
 		var pathString = toTrianglePath(5, 6, 7, 8);
 		expect(pathString).toBe('m 5 6 m 7 8 z');
-	})
+	});
 
 	it('should return a path string formatted for a triangle for negative numbers', function() {
 		var pathString = toTrianglePath(-1, -2, -3, -4);
 		expect(pathString).toBe('m -1 -2 m -3 -4 z');
-	})
+	});
 })
