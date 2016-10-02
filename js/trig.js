405d8b88d6014e1db54ad15c864a0ca67d9fdc33
Timothy Sugar <timothysugar@gmail.com>
2016-10-02 14:10:14 +0100

    Simple HTML page to accept a couple of inputs and print path string

--- js/trig.js
+++ js/trig.js
@@ -1,6 +1,13 @@
 var trig = (function () {
 	'use strict' 
 
+	function _buildPath() {
+		var lengthA = parseInt(document.getElementById('length-a').value);
+		var lengthB = parseInt(document.getElementById('length-b').value);
+		var resultPath = _toTrianglePath(lengthA, 0, 0, lengthB);
+		document.getElementById('path').value = resultPath;
+	}
+
 	function _toTrianglePath(a, b, c, d) {
 		if (_isNumber(a) && _isNumber(b) && _isNumber(c) && _isNumber(d)) {
 			return 'm ' + a + ' ' + b + ' m ' + c + ' ' + d + ' ' + 'z';
@@ -13,6 +20,7 @@ var trig = (function () {
 	}
 
 	return {
+		buildPath: _buildPath,
 		toTrianglePath: _toTrianglePath
 	};
 
